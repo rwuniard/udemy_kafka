3f89abfb44ff09b9cfe96506164fedb990196d54
Ronson Wuniardy <rwuniard@hotmail.com>
2022-09-08 18:31:42 -0400

    comment in consumer

--- beginner/firstpart/1-kafka-cli/2-kafka-console-consumer.sh
+++ beginner/firstpart/1-kafka-cli/2-kafka-console-consumer.sh
@@ -5,12 +5,17 @@ kafka-console-consumer.sh
 
 # consuming
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic
+# You won't see anything, until you send something from producer.
 
 # other terminal
 kafka-console-producer.sh --bootstrap-server localhost:9092 --topic first_topic
 
 # consuming from beginning
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --from-beginning
+# this is not by default to read from beginning, because it may have a tons of messages.
+# You will notice the order of the message doesn't match the order of the producer.
+# This is because the message is sent to a different partitions. The message within a partition is ordered.
+
 
 # display key, values and timestamp in consumer
 kafka-console-consumer --bootstrap-server localhost:9092 --topic first_topic --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true --from-beginning
